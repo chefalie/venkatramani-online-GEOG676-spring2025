cc0878c0a9666880d222c8836b8b61c60166fe89
chefalie <svenkatr@tamu.edu>
2025-01-16 17:15:52 -0600

    module 2 practice code
    created a new python to practice the code and concepts covered in module 2

---
+++ Practice/module2_practice.py
@@ -0,0 +1,43 @@
+#a = 1
+#b = (a*2)
+#n = 5
+#double_list = []
+
+#def doubling(a,b,n):
+    #count = 1
+    #double_list.append(a)
+
+    #while count <= n:
+        #double_list.append(b)
+        #a = b
+        #b = (a*2)
+        #count += 1
+    #print(double_list)
+
+#print(doubling(a,b,n))
+
+################################
+
+numOne = 100
+numTwo = 5
+operator = "9"
+
+if operator == "+":
+    result = (numOne + numTwo)
+elif operator == "-":
+    result = (numOne - numTwo)
+elif operator == "*":
+    result = (numOne * numTwo)
+elif operator == "/":
+    result = (numOne / numTwo)
+else:
+    result = ("Invalid operator") 
+
+print("Result =", result)
+
+
+
+
+
+
+
